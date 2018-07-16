35c956b6f102193f23e741c230368612a803b12b
Daniel <d.chen323@gmail.com>
2018-07-16 10:48:50 -0700

    Create counter component and install enzyme

---
+++ component/counter.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.add1 = this.add1.bind(this);
+  }
+
+  add1(){
+    this.setState(state => {
+      return {
+        count: state.count + 1
+      };
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <p>count: {this.state.count}</p>
+        <button onClick={this.add1}>Add One</button>
+      </div>
+    );
+  }
+}
--- directions.md
+++ directions.md
@@ -2,3 +2,9 @@ npm init --y
 npm install -jest
 write tests
 create .babelrc file to allow for es6 syntax
+import, export default
+
+use toEqual for testing objects
+
+npm install enzyme for React testing
+npm install enzyme-adapter-react-16
