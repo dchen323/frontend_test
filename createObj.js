0e4fa50f4e705b0ec56b01cd39ad28ee92b326c4
Daniel <d.chen323@gmail.com>
2018-07-16 10:41:54 -0700

    Create object test with toEqual

---
+++ createObj.js
@@ -0,0 +1,9 @@
+const createObj = (keys, vals) => {
+  let returnObj = {};
+  keys.forEach((key, idx) => {
+    returnObj[key] = vals[idx];
+  });
+  return returnObj;
+};
+
+export default createObj;
---
+++ directions.md
@@ -0,0 +1,4 @@
+npm init --y
+npm install -jest
+write tests
+create .babelrc file to allow for es6 syntax
