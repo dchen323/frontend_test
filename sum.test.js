fe9d657b9ef81b9d2e46a7f44c637be9c19fb163
Daniel <d.chen323@gmail.com>
2018-07-16 10:31:46 -0700

    Add babelrc to allow for es6 syntax

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,25 @@
-const sum = require('./sum');
+// const sum = require('./sum');
+import sum from './sum';
 
 describe('Sum', () => {
   it("adds 1 and 3 to get 4", () => {
     expect(sum(1,3)).toBe(4);
   });
+});
+
+describe('Sum', () => {
+  it("adds 1 and 3 to get 4", () => {
+    let val = sum(1,3);
+    expect(val).toBe(4);
+  });
+
+  it("makes sure adding 1 and 3 is not 100", () => {
+    let val = sum(1,3);
+    expect(val).not.toBe(100);
+  });
+});
+
+test('Truthiness', () => {
+  expect(false).toBeFalsy();
+  expect(true).toBeTruthy();
 });
